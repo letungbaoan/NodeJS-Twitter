1ede70a9281c760e9c6a3c10d306a368b3128c01
letungbaoan <baoan7604@gmail.com>
2024-01-25 00:31:52 +0700

    Add upload single image feature

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import usersRouter from './routes/users.routes'
 import databaseService from './services/database.service'
 import { defaultErrorHandler } from './middlewares/error.middewares'
+import mediasRouter from './routes/medias.routes'
 const app = express()
-const port = 3000
+const port = 4000
 databaseService.connect()
 app.use(express.json())
 
 app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
 
 app.use(defaultErrorHandler)
 app.listen(port, () => {
