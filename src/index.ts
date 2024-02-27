f7dbdf8c6391640b8e5789e95d6c3f19a750e05c
letungbaoan <baoan7604@gmail.com>
2024-02-27 21:43:27 +0700

    Feat: private message between two client

--- src/index.ts
+++ src/index.ts
@@ -50,10 +50,30 @@ const io = new Server(httpServer, {
 	}
 })
 
+const users: {
+	[key: string]: {
+		socket_id: string
+	}
+} = {}
+
 io.on('connection', (socket) => {
 	console.log(`${socket.id} user connected`)
+	const user_id = socket.handshake.auth._id
+	users[user_id] = {
+		socket_id: socket.id
+	}
+	console.log(users)
+	socket.on('private message', (data) => {
+		const receiver_socket_id = users[data.to].socket_id
+		socket.to(receiver_socket_id).emit('receive private message', {
+			content: data.content,
+			from: user_id
+		})
+	})
 	socket.on('disconnect', () => {
+		delete users[user_id]
 		console.log(`${socket.id} disconnected`)
+		console.log(users)
 	})
 })
 
