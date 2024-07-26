fe27bf1fbf2868a9c9e60cb30e776f36fb721fda
letungbaoan <baoan7604@gmail.com>
2024-07-26 11:17:48 +0700

    Change client URL

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -13,7 +13,7 @@ import { Server as ServerHttp } from 'http'
 const initSocket = (httpServer: ServerHttp) => {
 	const io = new Server(httpServer, {
 		cors: {
-			origin: 'http://localhost:3000'
+			origin: 'http://localhost:5000'
 		}
 	})
 
